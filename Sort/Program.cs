cb04b0830764e0da00427361c56b926f3d918eda
Николай Стригачев <nikolai7832@yandex.ru>
2022-05-19 12:22:27 +0300

    Добавьте файлы проекта.

---
+++ Sort/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // сортировка
+            var  sortedPhoneBook = phoneBook
+                                .OrderBy(s => s.Name)
+                                .ThenBy(s => s.LastName);
+
+            while (true)
+            {
+                char input = Console.ReadKey().KeyChar;
+                bool check = Int32.TryParse(input.ToString(), out int page);
+
+                if (!check || page < 1 || page > 3)
+                {
+                    Console.WriteLine("\nТакой страницы нет");
+                }
+                else
+                {
+                    var pageContent = sortedPhoneBook.Skip((page - 1) * 2).Take(2);
+
+                    Console.WriteLine();
+
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void ShowPage(List<Contact> book)
+        {
+
+        }
+    }
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
